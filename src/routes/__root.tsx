17c317f2700b120e53f8996586623d6c894bcbf3
saka-naname <reisuicupsoup@gmail.com>
2026-05-04 03:40:09 +0900

    feat: add Biome

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,27 +1,34 @@
-import { createRootRoute, HeadContent, Outlet, Scripts } from "@tanstack/react-router";
-import { ReactNode } from "react";
+import {
+	createRootRoute,
+	HeadContent,
+	Outlet,
+	Scripts,
+} from "@tanstack/react-router";
+import type { ReactNode } from "react";
 
 export const Route = createRootRoute({
-    head: () => ({
-
-    }),
-    component: RootComponent
-})
+	head: () => ({}),
+	component: RootComponent,
+});
 
 function RootComponent() {
-    return (<RootDocument><Outlet /></RootDocument>)
+	return (
+		<RootDocument>
+			<Outlet />
+		</RootDocument>
+	);
 }
 
-function RootDocument({children}: Readonly<{children: ReactNode}>) {
-    return (
-        <html>
-            <head>
-                <HeadContent />
-            </head>
-            <body>
-                { children }
-                <Scripts />
-            </body>
-        </html>
-    )
+function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
+	return (
+		<html lang="ja">
+			<head>
+				<HeadContent />
+			</head>
+			<body>
+				{children}
+				<Scripts />
+			</body>
+		</html>
+	);
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,9 @@
-import { createFileRoute } from "@tanstack/react-router"
+import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
-    component: Home
-})
+	component: Home,
+});
 
 function Home() {
-    return (
-        <>
-            <h1 className="text-4xl">Hello Tauri + Tanstack!</h1>
-        </>
-    )
+	return <h1 className="text-4xl">Hello Tauri + Tanstack!</h1>;
 }
