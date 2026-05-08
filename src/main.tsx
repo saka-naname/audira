9de72d552d118af22822c29e7747da55c7535165
saka-naname <reisuicupsoup@gmail.com>
2026-05-08 19:19:20 +0900

    feat: TanStack Query の追加

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles.css";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { queryClient } from "@/lib/query-client";
 import { routeTree } from "./routeTree.gen";
 
 const router = createRouter({
   routeTree,
   scrollRestoration: true,
+  context: {
+    queryClient,
+  },
 });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
